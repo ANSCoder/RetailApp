78400a5f0558ce4d06335633d288b8982516d2b2
ANSCoder <nimjea@gmail.com>
2025-02-01 11:47:05 +0530

    Initial Commit

---
+++ RetailApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RetailApp
+//
+//  Created by Anand Nimje on 01/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RetailApp/RetailAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RetailAppApp.swift
+//  RetailApp
+//
+//  Created by Anand Nimje on 01/02/25.
+//
+
+import SwiftUI
+
+@main
+struct RetailAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
